8bfac30985d017febb29ee916a5a630e0302cf5b
Simon O'Leary <Simon O'Leary>
2017-04-20 15:21:26 +0100

    Can now warp between spawn area and first map (and camera follows)

---
+++ oop/Unity2D/Assets/Scripts/Warp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+	
+	
+	public Transform WarpTarget;
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//Debug.Log("An object Collided");
+		
+		other.gameObject.transform.position = WarpTarget.position; //Changes player's destination to warptarget
+		Camera.main.transform.position = WarpTarget.position;	//Teleports camera to character instead of dragging it between maps
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
