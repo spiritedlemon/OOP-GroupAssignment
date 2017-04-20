92257e86070d47ea61c55c199e4f37bca561174f
Simon O'Leary <Simon O'Leary>
2017-04-20 21:55:47 +0100

    Added screen Fade out/in & added more folders to try clean up unity interface

---
+++ oop/Unity2D/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFader : MonoBehaviour {
+	
+	Animator anim;
+	bool isFading = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		
+		
+	}
+	
+	public IEnumerator FadeToClear()
+	{
+		isFading = true;
+		anim.SetTrigger("FadeIn");
+		
+		while (isFading)		//Infinitely run until criteria is met
+			yield return null;
+		
+	}
+	
+	public IEnumerator FadeToBlack()
+	{
+		isFading = true;
+		anim.SetTrigger("FadeOut");
+		
+		while (isFading)		//Infinitely run until criteria is met
+			yield return null;
+		
+	}
+	
+	
+	void AnimationComplete() // When animation complete - set isFading back to false
+	{
+		isFading = false;
+		
+	}
+	
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- oop/Unity2D/Assets/Scripts/Warp.cs
+++ oop/Unity2D/Assets/Scripts/Warp.cs
@@ -6,12 +6,24 @@ public class Warp : MonoBehaviour {
 	
 	public Transform WarpTarget;
 	
-	void OnTriggerEnter2D(Collider2D other)
+	IEnumerator OnTriggerEnter2D(Collider2D other)	//Changed to Enumerator to use yield statements
 	{
-		//Debug.Log("An object Collided");
+		ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader> ();
+		
+		//??.GetComponent<PlayerMovement>().enable = false; //Failed Attempt to freeze player for fade out 
+		
+		yield return StartCoroutine(sf.FadeToBlack () ); //Fades screen before player is moved
+		
+		
+		//Debug.Log("An object Collided"); //For De-bugging
 		
 		other.gameObject.transform.position = WarpTarget.position; //Changes player's destination to warptarget
 		Camera.main.transform.position = WarpTarget.position;	//Teleports camera to character instead of dragging it between maps
+		
+		
+		yield return StartCoroutine(sf.FadeToClear () ); //Clears screen after player has been moved
+		
+		//??.GetComponent<PlayerMovement>().enable = true; //Failed Attempt to freeze player for fade out 
 	}
 	
 	
