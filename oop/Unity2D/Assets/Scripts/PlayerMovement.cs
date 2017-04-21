121a0c03fcd5b22c437cf0bf55a1fcee3e3e245d
Simon O'Leary <Simon O'Leary>
2017-04-21 16:05:33 +0100

    Cant get the attack to work properly, will try again later/tomorrow

--- oop/Unity2D/Assets/Scripts/PlayerMovement.cs
+++ oop/Unity2D/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,10 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	
 	
-	Rigidbody2D rbody;
-	Animator anim;
+	private Rigidbody2D rbody;
+	private Animator anim;
 	
-	private bool attacking;
+	private bool attacking = false;
 	public float attTime; //How long player attacks for
 	private float attTimeCounter;
 
@@ -28,8 +28,8 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () 
 	{
 		
-		if(!attacking)
-		{
+		if(!attacking){
+			
 		Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") );
 			
 		if(movement_vector != Vector2.zero)
@@ -50,7 +50,7 @@ public class PlayerMovement : MonoBehaviour {
 		//Attacking Happens here
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			attTimeCounter = attTime;
+			attTimeCounter = attTime;	//attTime is set in unity - Under the the player object
 			attacking = true; 
 			rbody.velocity = Vector2.zero;
 			anim.SetBool("Attack", true);
@@ -59,6 +59,7 @@ public class PlayerMovement : MonoBehaviour {
 		
 		}//End of IfNotAttacking
 		
+		//Counter counts down to 0
 		if(attTimeCounter > 0)
 		{
 			
@@ -66,6 +67,7 @@ public class PlayerMovement : MonoBehaviour {
 			
 		}
 		
+		//At zero animation ends
 		if(attTimeCounter >= 0)
 		{
 			
