5a737520db95f3170d275b94533783408dd053e5
Simon O'Leary <Simon O'Leary>
2017-04-21 15:36:59 +0100

    Tried to get attacking working, some unkown error stopping it currently

--- oop/Unity2D/Assets/Scripts/PlayerMovement.cs
+++ oop/Unity2D/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,9 @@ public class PlayerMovement : MonoBehaviour {
 	Rigidbody2D rbody;
 	Animator anim;
 	
+	private bool attacking;
+	public float attTime; //How long player attacks for
+	private float attTimeCounter;
 
 	// Use this for initialization
 	void Start () 
@@ -24,8 +27,11 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") );
 		
+		if(!attacking)
+		{
+		Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") );
+			
 		if(movement_vector != Vector2.zero)
 		{
 			anim.SetBool ("iswalking", true);
@@ -41,5 +47,33 @@ public class PlayerMovement : MonoBehaviour {
 		
 		
 		
-	}
+		//Attacking Happens here
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			attTimeCounter = attTime;
+			attacking = true; 
+			rbody.velocity = Vector2.zero;
+			anim.SetBool("Attack", true);
+			
+		}
+		
+		}//End of IfNotAttacking
+		
+		if(attTimeCounter > 0)
+		{
+			
+			attTimeCounter -= Time.deltaTime;
+			
+		}
+		
+		if(attTimeCounter >= 0)
+		{
+			
+			attacking = false;
+			anim.SetBool("Attack", false);
+			
+		}
+		
+		
+	}//end of update
 }
