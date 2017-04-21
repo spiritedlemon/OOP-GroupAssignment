b6accd3e150d8d3169410be5e4b84487892d8057
Simon O'Leary <Simon O'Leary>
2017-04-21 14:28:20 +0100

    Added a couple scripts to give all enemies damage and to give our character HP

---
+++ oop/Unity2D/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,29 @@
+﻿//Script to control damage dealt to player by ANY enemy
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour {
+	
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			other.gameObject.GetComponent<PlayerHP>().HurtPlayer(damage);	//Pass damage to the HurtPlayer Fnc
+		}
+	}
+	
+}
---
+++ oop/Unity2D/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,41 @@
+﻿//Script to track player Current and max HP
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour {
+	
+	public int playerMaxHP = 100;
+	public int playerCurrentHP;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerCurrentHP = playerMaxHP; //At start of game fill HP
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if(playerCurrentHP <= 0)
+		{
+			gameObject.SetActive(false);
+		}
+		
+	}
+	
+	public void HurtPlayer(int damage)
+	{
+		playerCurrentHP = playerCurrentHP - damage;  //Deal damage to player
+	}
+	
+	public void SetMaxHP()
+	{
+		playerCurrentHP = playerMaxHP; //If you need to fill the player's hp (potions or something)
+	}
+	
+}
