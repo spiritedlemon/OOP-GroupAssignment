1d9645a9654ccc4ac96a7e1c88439038c60b16f1
craig <c15359071@mygit.ie>
2017-04-22 19:05:43 +0100

    player health bar added

---
+++ oop/Unity2D/Assets/Scripts/HealthLevelControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //for the user interface components such as a slider
+
+public class HealthLevelControl : MonoBehaviour {
+
+	//variables
+	public Slider healthbar;
+	public Texture HealthText;
+	public PlayerHP playerHealth;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		healthbar.maxValue = playerHealth.playerMaxHP;
+		healthbar.value = playerHealth.playerCurrentHP;
+	}
+}
