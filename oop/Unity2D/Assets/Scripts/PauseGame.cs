3e69c66cabb359c652af8e54f73fffbe8589cc92
craig <c15359071@mygit.ie>
2017-04-23 18:09:49 +0100

    pause menu now displaying when escape key is pressed

---
+++ oop/Unity2D/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	//variables 
+	public Transform PauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		//not needed in this script
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (PauseMenu.gameObject.activeInHierarchy == false) 
+			{
+				PauseMenu.gameObject.SetActive(true);
+			} else 
+			{
+				PauseMenu.gameObject.SetActive (false);
+			}
+		}
+	}
+}
