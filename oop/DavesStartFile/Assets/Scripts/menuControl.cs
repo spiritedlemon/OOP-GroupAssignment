aabdf7b2144331b88b552f56768e6c779b4e39bc
Simon O'Leary <Simon O'Leary>
2017-04-25 22:22:21 +0100

    Disabled ability to get upgraded sword - Cant get it working + Added daves start screen to the folder - will implement it now

---
+++ oop/DavesStartFile/Assets/Scripts/menuControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuControl : MonoBehaviour
+{
+    
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
--- oop/Unity2D/Assets/Scripts/UpgradeWeapon.cs
+++ oop/Unity2D/Assets/Scripts/UpgradeWeapon.cs
@@ -9,12 +9,12 @@ public class UpgradeWeapon : MonoBehaviour {
 	
 	public Collider2D Cave;
 	
-	private bool upgradeDone = false;
+	private bool upgradeDone;
 	
 	private Vector2 minPoint;
 	private Vector2 maxPoint;
 	
-	
+	//public GameObject player;
 	
 	// Use this for initialization
 	void Start () 
@@ -22,31 +22,35 @@ public class UpgradeWeapon : MonoBehaviour {
 		
 		//GameObject.FindWithTag("Weapon2").SetActive(false);
 		//	Debug.Log("Weapon2Disabled");
+		upgradeDone = false;
+		
+		if(Cave != null)	//Whole event only happens if area is present
+		{
+			minPoint = Cave.bounds.min;
+			maxPoint = Cave.bounds.max;
+		}
 	}
 	
 	
 	void OnTriggerEnter2D(Collider2D Other)
 	{
-		if(Cave != null)	//Whole event only happens if area is present
-		{
-			minPoint = Cave.bounds.min;
-			maxPoint = Cave.bounds.max;
-			
+		
 			//Can't of already got upgrade
 			if( (upgradeDone == false) && (transform.position.y < maxPoint.y) && (transform.position.y > minPoint.y) ) 
 			{
 				if( (transform.position.x < maxPoint.x) && (transform.position.x > minPoint.x) )
 				{
-					GameObject.FindWithTag("Weapon").SetActive(false);
-					Debug.Log("Weapon1Disabled");
-					GameObject.FindWithTag("Weapon2").SetActive(true);
-					Debug.Log("Weapon2Enabled");
+					//GameObject.Find("Sword1").SetActive(false);
+					//Debug.Log("Weapon1Disabled");
+					//GameObject.Find("Sword2").SetActive(true);
+					//GameObject.FindWithTag("Weapon2").SetActive(true); //Older attempt
+					//Debug.Log("Weapon2Enabled");
 					
-					upgradeDone = true;
+					print("Weapon upgrade diabled due to bugs - Can activate sword2 manually in hierarchy");
+					
+					//upgradeDone = true;
 				}
 			}
-			
-		}
 		
 		
 		
